1b934685af70d2c7798ae764adc50d4329f66210
Tuan Vuong <vanhtuan0409@gmail.com>
2025-04-18 15:42:04 +0700

    add dual attach or launch mode

--- src/main.ts
+++ src/main.ts
@@ -7,29 +7,34 @@ function debug(msg: string) {
   process.stderr.write(msg + "\n");
 }
 
-async function filterResponse(resp: Response): Promise<boolean> {
-  if (!resp.ok()) return false;
+function filterResponse(pattern?: string) {
+  return async function (resp: Response): Promise<boolean> {
+    if (!resp.ok()) return false;
 
-  const contentType = await resp
-    .headerValues("content-type")
-    .then((s) => s.join(","));
+    const contentType = await resp
+      .headerValues("content-type")
+      .then((s) => s.join(","));
 
-  return contentType === "application/octet-stream";
+    if (contentType !== "application/octet-stream") return false;
+    if (typeof pattern === "undefined" || pattern === "") return true;
+
+    return new RegExp(pattern).test(resp.url());
+  };
 }
 
-async function eventListener(resp: Response) {
-  if (await filterResponse(resp)) {
-    console.log(resp.url());
-  }
-}
-
-async function runInspect(page: Page, requestedUrl: string) {
-  page.on("response", eventListener);
+async function runInspect(page: Page, requestedUrl: string, pattern?: string) {
+  const filterFn = filterResponse(pattern);
+  page.on("response", async (resp) => {
+    if (await filterFn(resp)) {
+      console.log(resp.url());
+    }
+  });
   await page.goto(requestedUrl);
 }
 
-async function getFirst(page: Page, requestedUrl: string) {
-  const p = page.waitForResponse(filterResponse, { timeout: 30_000 });
+async function getFirst(page: Page, requestedUrl: string, pattern?: string) {
+  const filterFn = filterResponse(pattern);
+  const p = page.waitForResponse(filterFn, { timeout: 30_000 });
   await page.goto(requestedUrl);
   const resp = await p;
   console.log(resp.url());
@@ -38,13 +43,16 @@ async function getFirst(page: Page, requestedUrl: string) {
 async function main() {
   const { values: optValues, positionals } = parseArgs({
     options: {
+      attach: { type: "boolean", default: false },
+      chromePort: { type: "string", default: "9222" },
       inspect: { type: "boolean", short: "i", default: false },
+      match: { type: "string", short: "m", default: "" },
       help: { type: "boolean", short: "h", default: false },
     },
     allowPositionals: true,
   });
   if (optValues.help) {
-    debug("fpt-sub-extract <input_url> [--inspect]");
+    debug("fpt-sub-extract <input_url> [--inspect] [--attach] [-m <pattern>]");
     return;
   }
 
@@ -52,18 +60,23 @@ async function main() {
   if (typeof requestedUrl === "undefined") throw new Error("missing input url");
   debug(`using chrome bin ${CHROME_BIN}`);
   debug(`processing link ${requestedUrl}`);
+  debug(`opts: ${JSON.stringify(optValues)}`);
 
-  const browser = await chromium.launch({
-    executablePath: CHROME_BIN,
-    headless: true,
-  });
-  const context = await browser.newContext();
+  const browser = optValues.attach
+    ? await chromium.connectOverCDP(`http://127.0.0.1:${optValues.chromePort}`)
+    : await chromium.launch({
+      executablePath: CHROME_BIN,
+      headless: true,
+    });
+  const context = optValues.attach
+    ? browser.contexts()[0]
+    : await browser.newContext();
   const page = await context.newPage();
 
   if (optValues.inspect) {
-    await runInspect(page, requestedUrl);
+    await runInspect(page, requestedUrl, optValues.match);
   } else {
-    await getFirst(page, requestedUrl);
+    await getFirst(page, requestedUrl, optValues.match);
     await browser.close();
   }
 }
